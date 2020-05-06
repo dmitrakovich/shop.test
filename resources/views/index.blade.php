e6c3ae3566c76550b8890cef513c201f91aeb97b
Андрей Дмитракович <dmitrakovich.andrey@yandex.by>
2020-05-06 12:52:42 +0300

    add fonts

---
+++ resources/views/index.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            {{-- <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    You are logged in!
+                </div>
+            </div> --}}
+
+            <div class="col-12">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. A, sint dolorum doloremque saepe minus consequuntur beatae fugiat ipsam doloribus, ipsum nemo amet modi mollitia similique maxime possimus minima voluptatibus? Aperiam.
+            </div>
+            <div class="col-12"><hr></div>
+            <div class="col-12 test-roboto">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. A, sint dolorum doloremque saepe minus consequuntur beatae fugiat ipsam doloribus, ipsum nemo amet modi mollitia similique maxime possimus minima voluptatibus? Aperiam.
+            </div>
+            <div class="col-12"><hr></div>
+            <div class="col-12 test-popins">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. A, sint dolorum doloremque saepe minus consequuntur beatae fugiat ipsam doloribus, ipsum nemo amet modi mollitia similique maxime possimus minima voluptatibus? Aperiam.
+            </div>
+            <div class="col-12"><hr></div>
+            <div class="col-12 test-gilroy">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. A, sint dolorum doloremque saepe minus consequuntur beatae fugiat ipsam doloribus, ipsum nemo amet modi mollitia similique maxime possimus minima voluptatibus? Aperiam.
+            </div>
+            <div class="col-12"><hr></div>
+            <div class="col-12 test-gill-sans-mt">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. A, sint dolorum doloremque saepe minus consequuntur beatae fugiat ipsam doloribus, ipsum nemo amet modi mollitia similique maxime possimus minima voluptatibus? Aperiam.
+            </div>
+
+
+
+        </div>
+    </div>
+</div>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,10 +14,6 @@
     <script src="{{ mix('js/vendor.js') }}" defer></script>
     <script src="{{ mix('js/app.js') }}" defer></script>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
     <!-- Styles -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
