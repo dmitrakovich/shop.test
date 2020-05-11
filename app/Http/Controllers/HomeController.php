fad4285d548643ea1997e0c8c7b7a6f17cb98cbe
Андрей Дмитракович <dmitrakovich.andrey@yandex.by>
2020-05-11 11:57:30 +0300

    test log messages

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class HomeController extends Controller
 {
@@ -13,7 +14,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        // $this->middleware('auth');
     }
 
     /**
@@ -23,6 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
+        Log::info('Test log message with data', ['id' => 123]);
         return view('home');
     }
 }
