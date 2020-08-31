392333ec0a96a408f61520602c4a46fde23bc633
Андрей Дмитракович <dmitrakovich.andrey@yandex.by>
2020-08-31 10:43:19 +0300

    make observers

---
+++ app/Observers/OrderObserver.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Observers;
+
+use App\Order;
+
+class OrderObserver
+{
+    /**
+     * Handle the order "created" event.
+     *
+     * @param  \App\Order  $order
+     * @return void
+     */
+    public function created(Order $order)
+    {
+        //
+    }
+
+    /**
+     * Handle the order "updated" event.
+     *
+     * @param  \App\Order  $order
+     * @return void
+     */
+    public function updated(Order $order)
+    {
+        //
+    }
+
+    /**
+     * Handle the order "deleted" event.
+     *
+     * @param  \App\Order  $order
+     * @return void
+     */
+    public function deleted(Order $order)
+    {
+        //
+    }
+
+    /**
+     * Handle the order "restored" event.
+     *
+     * @param  \App\Order  $order
+     * @return void
+     */
+    public function restored(Order $order)
+    {
+        //
+    }
+
+    /**
+     * Handle the order "force deleted" event.
+     *
+     * @param  \App\Order  $order
+     * @return void
+     */
+    public function forceDeleted(Order $order)
+    {
+        //
+    }
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Observers\UserObserver;
+use App\User;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -25,5 +27,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        User::observe(UserObserver::class);
     }
 }
