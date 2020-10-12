2a388334bd2d894339d4916c5a9a26420fd78bf5
Андрей Дмитракович <dmitrakovich.andrey@yandex.by>
2020-10-12 12:28:23 +0300

    fix paginator for laravel 8

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Observers\UserObserver;
 use App\Repositories\CategoryRepository;
 use App\User;
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
@@ -29,6 +30,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        Paginator::useBootstrap();
 
         User::observe(UserObserver::class);
 
