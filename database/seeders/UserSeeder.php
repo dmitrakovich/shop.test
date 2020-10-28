c447f1ba849f81664a5179c97f3eb2c399fb2ede
Андрей Дмитракович <dmitrakovich.andrey@yandex.by>
2020-10-28 11:45:51 +0300

    import actual user data in user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -7,6 +7,9 @@ use Illuminate\Support\Facades\DB;
 
 class UserSeeder extends Seeder
 {
+    protected string $tableName = 'users';
+    protected string $oldTableName = 'cyizj_jshopping_users';
+    protected string $oldExtTableName = 'cyizj_users';
     /**
      * Run the database seeds.
      *
@@ -14,27 +17,35 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->truncate();
+        DB::table($this->tableName)->truncate();
 
-        $importDataFile = database_path() . '/import/cyizj_users.php';
-        if (file_exists($importDataFile)) {
-            require_once $importDataFile;
-            if (isset($cyizj_users) && is_array($cyizj_users)) {
-                foreach ($cyizj_users as $user) {
-                    if (empty($user['email']) || empty($user['password'])) {
-                        continue;
-                    }
-                    $data[$user['email']] = [
-                        'first_name' => $user['name'] ?? '',
-                        'email' => $user['email'],
-                        'email_verified_at' => now(),
-                        'password' => $user['password'],
-                        'created_at' => now(),
-                        'updated_at' => now(),
-                    ];
-                }
+        $oldUsers = DB::connection('old_mysql')
+            ->table($this->oldTableName)
+            ->leftJoin($this->oldExtTableName, "$this->oldTableName.user_id", '=', "$this->oldExtTableName.id")
+            ->get([
+                $this->oldTableName .'.user_id as id',
+                $this->oldTableName .'.email',
+                $this->oldTableName .'.f_name as first_name',
+                $this->oldTableName .'.l_name as last_name',
+                $this->oldTableName .'.m_name as patronymic_name',
+                $this->oldTableName .'.mobil_phone as phone',
+                // 'birthday as birth_date', // везде пусто
+                // 'country_id',
+                $this->oldTableName .'.street as address',
+                $this->oldExtTableName .'.password',
+                $this->oldExtTableName . '.registerDate as created_at',
+                $this->oldExtTableName . '.lastvisitDate as updated_at',
+            ]);
+
+        foreach ($oldUsers as $oldUser) {
+            if (empty($oldUser->email) || empty($oldUser->password)) {
+                continue;
             }
+            if ($oldUser->updated_at == '0000-00-00 00:00:00') {
+                $oldUser->updated_at = null;
+            }
+            
+            DB::table($this->tableName)->insert((array)$oldUser);
         }
-        DB::table('users')->insert($data);
     }
 }
