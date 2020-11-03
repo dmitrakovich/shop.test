76c7452bd323c82ce90b624c96974ebb2372aa1a
Андрей Дмитракович <dmitrakovich.andrey@yandex.by>
2020-11-03 08:51:08 +0300

    feedbacks migrations

---
+++ database/migrations/2020_10_28_115506_create_feedback_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFeedbackTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('feedback', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->nullable()->index();
+            $table->unsignedBigInteger('yandex_id')->nullable()->index();
+            $table->string('user_name');
+            $table->string('user_email');
+            $table->unsignedBigInteger('user_phone')->nullable();
+            $table->text('text');
+            $table->tinyInteger('rating')->default(0);
+            $table->unsignedBigInteger('product_id')->index()->default(0);
+            $table->tinyInteger('type_id')->default(1);
+            $table->boolean('view_only_posted')->default(true);
+            $table->boolean('publish')->default(true);
+            $table->ipAddress('ip');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('feedback');
+    }
+}
