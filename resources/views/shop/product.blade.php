7fa6a8246dabbd2d24710b2329dd2c40d0e94824
Андрей Дмитракович <dmitrakovich.andrey@yandex.by>
2020-11-19 11:35:50 +0300

    product page layout

--- resources/views/shop/product.blade.php
+++ resources/views/shop/product.blade.php
@@ -5,12 +5,15 @@
 @section('breadcrumbs', Breadcrumbs::render('product', $product))
 
 @section('content')
-    <div class="col-12">
+    <div class="col-12 product-page">
         <div class="row">
             <div class="col-12 col-md-6 col-xl-7">
-                <img src="{{ $product->getFirstMedia()->getUrl('full') }}" alt="{{ $product->title }}" class="img-fluid"><br>
+                {{-- <img src="{{ $product->getFirstMedia()->getUrl('full') }}" alt="{{ $product->title }}" --}}
+                <img src="{{ $product->getFirstMedia()->getUrl('normal') }}" alt="{{ $product->title }}"
+                    class="img-fluid"><br>
                 @foreach ($product->getMedia() as $image)
-                    <img src="{{ $image->getUrl('thumb') }}" alt="{{ $product->title }}" class="img-fluid pr-3" style="max-width: 70px">
+                    <img src="{{ $image->getUrl('thumb') }}" alt="{{ $product->title }}" class="img-fluid pr-3"
+                        style="max-width: 70px">
                 @endforeach
             </div>
             <div class="col-12 col-md-6 col-xl-5">
@@ -21,50 +24,87 @@
                         <div class="col-6 text-muted">
                             {{ $product->getFullName() }}
                         </div>
-                        <div class="col-6">
-                            рейтинг
+                        <div class="col-6 text-right rating-result">
+                            @for ($i = 1; $i <= 5; $i++)
+                                <span class="star {{-- $frating >= $i ? 'active' : '' --}}"></span>
+                            @endfor
+
+                            <span class="ml-2 align-text-bottom">(0)</span>
                         </div>
                     </div>
-    
-                    <div class="row">
-                        <div class="col-auto">
-                            <s>{{ $product->old_price }}</s>
-                            {{ $product->price }}
+                </div>
+
+                <div class="col-12 col-xl-10">
+                    <div class="row mt-4">
+                        <div class="col-12 col-lg-6 price-block">
+                            <div class="row">
+                                {{-- @if ($product->price < $product->old_price)
+                                    <div class="col-auto price price-old">
+                                        <s>{{ $product->old_price }}</s>
+                                    </div>
+                                    <div class="col-auto price price-new">
+                                        {{ $product->price }}
+                                    </div>
+                                    @else
+                                    <div class="col-auto price price-new">
+                                        {{ $product->price }}
+                                    </div>
+                                @endif --}}
+
+                                @if ($product->price < $product->old_price)
+                                    <div class="col-auto price price-old">
+                                        {{ $product->old_price }}
+                                    </div>
+                                @endif
+                                <div class="col-auto price price-new">
+                                    {{ $product->price }} руб.
+                                </div>
+                            </div>
+
                         </div>
-                        <div class="col-auto">
+                        <div class="col-12 col-lg-6 text-right">
                             Условия рассрочки
+                            <div class="tooltip-trigger ml-2">?</div>
                         </div>
                     </div>
-    
-                    <div class="row">
-                        <div class="col-12 py-3">
+
+                    <div class="row my-4">
+                        <div class="col-12 py-3 py-xl-4 text-center" style="background: #EEF6FC;">
                             -30% на вторую и -50% на третью пару до конца апреля
                         </div>
                     </div>
-    
+
                     <div class="row">
                         <div class="col-12 py-3">
                             Цвет / {{ $product->color->name }}
                         </div>
                     </div>
-    
-                    <div class="row">
-                        <div class="product-size">
-                            <p><b>Выберите размер:</b> <a href="#">Таблица размеров</a></p>
-                            <ul>
+
+                    <div class="row mb-4">
+                        <div class="col-12 product-size">
+                            <div class="row justify-content-between">
+                                <div class="col-auto">
+                                    <span class="text-muted">Выберите размер:</span>
+                                </div>
+                                <div class="col-auto">
+                                    <a class="text-decoration-underline" href="#">Таблица размеров</a>
+                                </div>
+                            </div>
+                            <ul class="p-0 mt-3">
                                 @foreach ($product->sizes as $size)
-                                    <li>
+                                    <li class="d-inline-block pr-3">
                                         <label class="check">
-                                            <input type="checkbox"><i class="checkmark">{{ $size->name }}</i>
+                                            <input type="checkbox" class="d-none">
+                                            <span class="checkmark">{{ $size->name }}</span>
                                         </label>
                                     </li>
                                 @endforeach
                             </ul>
                         </div>
                     </div>
-    
+
                     <div class="row justify-content-center">
-                        <div class="col-12 col-lg-8">
+                        <div class="col-12">
                             <button class="btn btn-dark btn-lg btn-block py-3 js-add-to-cart">
                                 В КОРЗИНУ
                             </button>
@@ -73,15 +113,18 @@
                             </button>
                         </div>
                     </div>
-    
-                    <div class="col-12 text-center mt-5">
+
+                    <div class="col-12 text-center text-muted mt-5">
                         <p>
+                            <img src="/images/icons/installments.svg" class="pr-2">
                             Без переплат в рассрочку
                         </p>
                         <p>
+                            <img src="/images/icons/delivery.svg" class="pr-2">
                             Примерка по Беларуси
                         </p>
                         <p>
+                            <img src="/images/icons/return.svg" class="pr-2">
                             Возврат 14 дней
                         </p>
                     </div>
@@ -90,23 +133,49 @@
             </div>
         </div>
 
-        <div class="row my-5">
-            <div class="col-7">
-                ОПИСАНИЕ <br>
+        <div class="row my-5 product-description">
+            <div class="col-12 font-size-15 mb-1">
+                ОПИСАНИЕ
+            </div>
+            <div class="col-12 col-lg-7">
                 {!! $product->description !!}
             </div>
-            <div class="col-4 offset-1">
-                Цвет - {{ $product->color->name }} <br>
-                Сезон - {{ $product->season->name }} <br>
-                Материал верха - {{ $product->fabric_top_txt }} <br>
-                Внутренний материал - {{ $product->fabric_inner_txt }} <br>
-                Материал стельки - {{ $product->fabric_insole_txt }} <br>
-                Материал подошвы - {{ $product->fabric_outsole_txt }} <br>
-                Высота каблука - {{ $product->heel_txt }} <br>
-                Артикул - {{ $product->title }} <br>
+            <div class="col-12 col-lg-4 offset-lg-1">
+                @if (!empty($product->color->name))
+                    Цвет - {{ $product->color->name }} <br>
+                @endif
+
+                @if (!empty($product->season))
+                    Сезон - {{ $product->season->name }} <br>
+                @endif
+
+                @if (!empty($product->fabric_top_txt))
+                    Материал верха - {{ $product->fabric_top_txt }} <br>
+                @endif
+
+                @if (!empty($product->fabric_inner_txt))
+                    Внутренний материал - {{ $product->fabric_inner_txt }} <br>
+                @endif
+
+                @if (!empty($product->fabric_insole_txt))
+                    Материал стельки - {{ $product->fabric_insole_txt }} <br>
+                @endif
+
+                @if (!empty($product->fabric_outsole_txt))
+                    Материал подошвы - {{ $product->fabric_outsole_txt }} <br>
+                @endif
+
+                @if (!empty($product->heel_txt))
+                    Высота каблука - {{ $product->heel_txt }} <br>
+                @endif
+
+                @if (!empty($product->title))
+                    Артикул - {{ $product->title }} <br>
+                @endif
+
             </div>
         </div>
 
     </div>
-    
+
 @endsection
