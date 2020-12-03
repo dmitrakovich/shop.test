5d5177619c45c4ba76e4c1e340334a217fcd25e2
Андрей Дмитракович <dmitrakovich.andrey@yandex.by>
2020-12-03 07:59:11 +0300

    remove darryldecode/cart library

--- config/app.php
+++ config/app.php
@@ -175,7 +175,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Drandin\DeclensionNouns\DeclensionNounsServiceProvider::class,
-        Darryldecode\Cart\CartServiceProvider::class,
 
     ],
 
@@ -229,7 +228,6 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'DeclensionNoun' => \Drandin\DeclensionNouns\Facades\DeclensionNoun::class,
-        'Cart' => Darryldecode\Cart\Facades\CartFacade::class,
         'Banner' => App\Models\Banner::class,
 
     ],
