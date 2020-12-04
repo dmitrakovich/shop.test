08454350058f3f3c82bc5319e842b3c294b5bb92
Андрей Дмитракович <dmitrakovich.andrey@yandex.by>
2020-12-04 13:05:19 +0300

    cart tables

---
+++ database/migrations/2020_12_03_080136_create_carts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCartsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carts', function (Blueprint $table) {
+            $table->id();
+            $table->enum('order_type', ['wholesale', 'retail']);
+            $table->foreignId('promocode_id')->nullable();
+            $table->boolean('cancel_promocode')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carts');
+    }
+}
---
+++ database/migrations/2020_12_03_080200_create_cart_data_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCartDataTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cart_data', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('cart_id')->index();
+            $table->foreignId('product_id');
+            $table->integer('count');
+            $table->foreignId('size_id');
+            $table->foreignId('color_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cart_data');
+    }
+}
