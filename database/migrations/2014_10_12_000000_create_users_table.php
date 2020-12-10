a5f18a4e8d7762fba4af06459ae7d54f82fc452d
Андрей Дмитракович <dmitrakovich.andrey@yandex.by>
2020-12-10 11:56:27 +0300

    add cart token in users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('cart_token')->nullable()->unique();
             $table->string('email')->unique();
             $table->string('first_name');
             $table->string('last_name')->nullable();
