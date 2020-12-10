a91611a4db0ad8bd18c78b2ec2107c2177970722
Андрей Дмитракович <dmitrakovich.andrey@yandex.by>
2020-12-10 12:58:43 +0300

    add to cart & cart items count methods

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -6,39 +6,88 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cookie;
+use ReflectionClass;
 
 class Cart extends Model
 {
     use HasFactory;
-
+    /**
+     * Инициализация корзины
+     *
+     * @return $this
+     */
     public function setCart()
     {
-
-
-        /*dd(
-            Auth::user()
-        );
-
-        $cartId = User::IsAuth() ?  null;
-
-
-        if (User::IsAuth()) {
-            $cart_id = User::get()->cart_id;
-        } else if (Cookie::has('cart_token')) {
-            $cart_id = Cookie::get('cart_token');
-        }
-        return Cart::where('id', $cart_id)->first() ?? new Cart();*/
-        return new Cart();
+        $cartId = Auth::user() ? Auth::user()->cart_token : Cookie::get('cart_token');
+        return Cart::findOrNew($cartId);
     }
-
-    public function data()
+    /**
+     * Содержимое корзины
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function items()
     {
-        $this->hasMany(CartData::class);
+        return $this->hasMany(CartData::class);
     }
-
+    /**
+     * Количество товаров в корзине
+     *
+     * @return int
+     */
     public function itemsCount()
     {
-        return 5;
-        // return count($this->items);
+        $counter = 0;
+        foreach ($this->items as $item) {
+            $counter += $item->count;
+        }
+        return $counter;
+    }
+    /**
+     * Добавить товар в корзину
+     *
+     * @param integer $productId
+     * @param integer $sizeId
+     * @param integer $colorId
+     * @return void
+     */
+    public function addItem(int $productId, int $sizeId, int $colorId)
+    {
+        $this->createIfNotExists();
+
+        $item = $this->items
+            ->where('product_id', $productId)
+            ->where('size_id', $sizeId)
+            ->where('color_id', $colorId)
+            ->first();
+
+        if (isset($item)) {
+            $item->increment('count');
+        } else {
+            $this->items()->create([
+                'product_id' => $productId,
+                'count' => 1,
+                'size_id' => $sizeId,
+                'color_id' => $colorId
+            ]);
+        }
+    }
+    /**
+     * Создать корзину, если она еще не создана
+     *
+     * @return void
+     */
+    public function createIfNotExists()
+    {
+        if (!$this->exists) {
+            $this->save();
+            if (Auth::check()) {
+                $user = Auth::user();
+                $user->cart_token = $this->id;
+                $user->save();
+            } else {
+                Cookie::queue(cookie('cart_token', $this->id, 60 * 24 * 30, '/'));
+            }
+        }
     }
 }
--- app/Models/CartData.php
+++ app/Models/CartData.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class CartData extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'cart_id',
+        'product_id',
+        'size_id',
+        'color_id',
+        'count',
+    ];
 }
