da3f17ff2ef9032b918116a3ed0bed7d97dd688b
Андрей Дмитракович <dmitrakovich.andrey@yandex.by>
2020-12-28 17:51:21 +0300

    video banner

--- resources/views/banners/index-main.blade.php
+++ resources/views/banners/index-main.blade.php
@@ -1,4 +1,4 @@
-<div class="col-12 px-main py-main d-none d-lg-block">
+{{-- <div class="col-12 px-main py-main d-none d-lg-block">
     <a href="{{ route('shop') }}">
         <img src="/images/banners/{{ $banners['main'] }}" alt="" class="img-fluid">
     </a>
@@ -22,4 +22,13 @@
     <a href="{{ route('shop') }}">
         <img src="/images/banners/{{ $banners[2] }}" alt="" class="img-fluid">
     </a>
+</div> --}}
+
+<div class="col-12 px-0">
+    <video id="videoblock" class="img-fluid" autoplay loop preload="metadata" muted playsinline
+        poster="/videos/201016_vitacci{{ Agent::isMobile() ? '_m' : null }}.jpg">
+        <source src="/videos/201016_vitacci.mp4" type="video/mp4" />
+        <source src="/videos/201016_vitacci.webm" type="video/webm" />
+        <source src="/videos/201016_vitacci.ogv" type="video/ogg" />
+    </video>
 </div>
