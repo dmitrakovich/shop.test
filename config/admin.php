438376b1a3e47ab8c05c68a189467afab4b42097
Андрей Дмитракович <dmitrakovich.andrey@yandex.by>
2021-02-02 10:02:24 +0300

    install laravel-admin-ext/ckeditor

--- config/admin.php
+++ config/admin.php
@@ -396,6 +396,15 @@ return [
     |
     */
     'extensions' => [
+        'ckeditor' => [
 
+            //Set to false if you want to disable this extension
+            'enable' => true,
+
+            // Editor configuration
+            'config' => [
+
+            ]
+        ]
     ],
 ];
