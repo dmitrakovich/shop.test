931d400046c5eae2099b5d1f163a096e979febac
Andrey Dmitrakovich <dmitrakovich.andrey@yandex.by>
2021-05-24 00:28:17 +0300

    add "Restore" action in admin panel

---
+++ app/Admin/Actions/Post/BatchRestore.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Admin\Actions\Post;
+
+use Encore\Admin\Actions\BatchAction;
+use Illuminate\Database\Eloquent\Collection;
+
+class BatchRestore extends BatchAction
+{
+    public $name = 'Восстановить';
+
+    public function handle (Collection $collection)
+    {
+        $collection->each->restore();
+
+        return $this->response()->success('Восстановлены')->refresh();
+    }
+
+    public function dialog ()
+    {
+        $this->confirm('Вы уверены, что хотите восстановить?');
+    }
+}
---
+++ app/Admin/Actions/Post/Restore.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Admin\Actions\Post;
+
+use Encore\Admin\Actions\RowAction;
+use Illuminate\Database\Eloquent\Model;
+
+class Restore extends RowAction
+{
+    public $name = 'Восстановить';
+
+    public function handle (Model $model)
+    {
+        $model->restore();
+
+        return $this->response()->success('Восстановлено')->refresh();
+    }
+
+    public function dialog()
+    {
+        $this->confirm('Вы уверены, что хотите восстановить?');
+    }
+}
