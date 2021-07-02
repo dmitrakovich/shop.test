6cd957ebecb54c8c1bad780fe802643be6726657
Andrey Dmitrakovich <dmitrakovich.andrey@yandex.by>
2021-07-02 08:46:07 +0300

    check isset catalog top banner

--- resources/views/banners/catalog-top.blade.php
+++ resources/views/banners/catalog-top.blade.php
@@ -1,9 +1,11 @@
-<div class="col-12 d-none d-lg-block">
-    <a href="{{ $catalogBanner->url }}">
-        <img src="{{ $catalogBanner->getFirstMediaUrl() }}"
-            alt="{{ $catalogBanner->title }}"
-            title="{{ $catalogBanner->title }}"
-            class="img-fluid"
-        />
-    </a>
-</div>
+@if (isset($catalogBanner))
+    <div class="col-12 d-none d-lg-block">
+        <a href="{{ $catalogBanner->url }}">
+            <img src="{{ $catalogBanner->getFirstMediaUrl() }}"
+                alt="{{ $catalogBanner->title }}"
+                title="{{ $catalogBanner->title }}"
+                class="img-fluid"
+            />
+        </a>
+    </div>
+@endif
