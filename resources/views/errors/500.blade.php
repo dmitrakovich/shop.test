83286b4a146ad5ccd89f1b19b8652373041e82ad
Andrey Dmitrakovich <dmitrakovich.andrey@yandex.by>
2021-07-20 13:36:22 +0300

    user feedback for sentry translate

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -12,7 +12,18 @@
       <script>
         Sentry.init({ dsn: 'https://e75029f28a974691925616a8d3c3a4a0@o923846.ingest.sentry.io/5871606' });
         Sentry.showReportDialog({
-          eventId: '{{ app('sentry')->getLastEventId() }}'
+          eventId: '{{ app('sentry')->getLastEventId() }}',
+          title: "Похоже, у нас проблемы.",
+          subtitle: "Наша команда получила уведомление.",
+          subtitle2: "Если вы хотите помочь, расскажите нам, что произошло.",
+          labelName: "{{ __('auth.Name') }}",
+          labelEmail: "{{ __('auth.E-Mail Address') }}",
+          labelComments: "Что случилось?",
+          labelClose: "{{ __('admin.close') }}",
+          labelSubmit: "{{ __('admin.submit') }}",
+          errorGeneric: "При отправке вашего отчета произошла неизвестная ошибка. Пожалуйста, попробуйте еще раз.",
+          errorFormEntry: "Некоторые поля были заполнены некорректно. Пожалуйста, исправьте ошибки и попробуйте еще раз.",
+          successMessage: "Ваш отзыв был отправлен. Спасибо!"
         });
       </script>
     @endif
