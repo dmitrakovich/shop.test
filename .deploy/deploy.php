35a2e1ee34217e4b9d56395aee80fa4877fb6fb3
Andrey Dmitrakovich <dmitrakovich.andrey@yandex.by>
2021-08-08 00:49:51 +0300

    deploy on production

--- .deploy/deploy.php
+++ .deploy/deploy.php
@@ -4,43 +4,72 @@ namespace Deployer;
 
 require 'recipe/laravel.php';
 
-task('test', function () {
-    writeln('Hello world');
-});
-
-/*
 // Project name
-set('application', 'my_project');
+set('application', 'Barocco');
 
 // Project repository
 set('repository', 'https://github.com/default-089/shop.test.git');
 
+// Hosts
+host('barocco.by')
+    ->set('public_html', '~/{{application}}');
+
 // [Optional] Allocate tty for git clone. Default value is false.
-set('git_tty', true); 
+// set('git_tty', true); 
 
 // Shared files/dirs between deploys 
-add('shared_files', []);
-add('shared_dirs', []);
+add('shared_files', [
+    '.env'
+]);
+add('shared_dirs', [
+    'storage/app/public',
+    'storage/app/temp',
+    'storage/framework/sessions',
+    'storage/logs',
+    'public/uploads',
+    'database/files',
+    'database/sxgeo',
+]);
 
 // Writable dirs by web server 
-add('writable_dirs', []);
-set('allow_anonymous_stats', false);
+add('writable_dirs', [
+    'storage',
+    'bootstrap/cache'
+]);  
 
-// Hosts
 
-host('project.com')
-    ->set('deploy_path', '~/{{application}}');    
-    
 // Tasks
 
-task('build', function () {
-    run('cd {{release_path}} && build');
+// task('build', function () {
+//     run('cd {{release_path}} && build');
+// });
+
+task('deploy:update_code', function () {
+	upload('src/', '{{release_path}}/');
 });
+
+// Migrate database before symlink new release.
+
+task('deploy', [
+    'deploy:prepare',
+    'deploy:lock',
+    'deploy:release',
+    'deploy:update_code',
+    'deploy:shared',
+    'deploy:writable',
+    // 'deploy:vendors',
+    // 'deploy:clear_paths',
+    'deploy:symlink',
+    'deploy:unlock',
+    'cleanup',
+    'success'
+]);
 
 // [Optional] if deploy fails automatically unlock.
 after('deploy:failed', 'deploy:unlock');
 
-// Migrate database before symlink new release.
+// before('deploy:symlink', 'artisan:migrate');
 
-before('deploy:symlink', 'artisan:migrate');
-*/
+// Deployer config
+set('keep_releases', 4);
+set('allow_anonymous_stats', false);
