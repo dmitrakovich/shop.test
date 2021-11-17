ad984dcc0f67ebbb034c81fe733d115415de4a9a
Andrey Dmitrakovich <dmitrakovich.andrey@yandex.by>
2021-11-17 10:59:19 +0300

    make OrderService

--- src/app/Http/Controllers/Shop/OrderController.php
+++ src/app/Http/Controllers/Shop/OrderController.php
@@ -6,10 +6,10 @@ use App\Facades\Cart;
 use App\Facades\Sale;
 use App\Models\Order;
 use App\Models\CartData;
-use App\Facades\Currency;
 use App\Events\OrderCreated;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Contracts\OrderServiceIntarface;
 use App\Http\Requests\StoreOrderRequest;
 use Illuminate\Database\Eloquent\Collection as EloquentCollection;
 
@@ -45,7 +45,6 @@ class OrderController extends BaseController
      */
     public function store(StoreOrderRequest $request)
     {
-        $userData = $request->validated();
         $isOneClick = $request->has(['product_id', 'sizes']);
 
         if ($isOneClick) {
@@ -65,50 +64,11 @@ class OrderController extends BaseController
         }
         Sale::applyForCart($cart);
 
-        $order = Order::create([
-            'user_name' => $userData['name'],
-            'user_id' => Auth::check() ? Auth::id() : null,
-            'type' => 'retail',
-            'email' => $userData['email'] ?? null,
-            'phone' => $userData['phone'],
-            // 'comment' => $request->input('comment'),
-            'currency' => Currency::getCurrentCurrency()->code,
-            'rate' => Currency::getCurrentCurrency()->rate,
-            // 'promocode_id' => Cart::getPromocodeId(),
-            // 'country' => $address['country'] ?? null,
-            // 'region' => $address['administrative_area_level_1'] ?? null,
-            'city' => $userData['city'] ?? null,
-            // 'zip' => $address['postal_code'] ?? null,
-            // 'street' => $address['route'] ?? null,
-            // 'house' => $address['street_number'] ?? null,
-            'user_addr' => $userData['user_addr'] ?? null,
-            'payment' => $userData['payment_name'],
-            'payment_code' => $userData['payment_code'],
-            // 'payment_cost' => isset($payment['price']) ? $payment['price'] : 0.00,
-            'delivery' => $userData['delivery_name'],
-            'delivery_code' => $userData['delivery_code'],
-            // 'delivery_cost' => $delivery['price'] ?? null,
-            // 'delivery_point' => $point['address'] ?? '',
-            // 'delivery_point_code' => $point['code'] ?? '',
-            // 'source' => Cookie::has('soc_order') ? 1 : 0
-        ]);
-
-        foreach ($cart->items as $item) {
-            $order->data()->create([
-                'product_id' => $item->product_id,
-                'size_id' => $item->size_id,
-                'count' => $item->count,
-                'buy_price' => $item->product->buy_price,
-                'price' => $item->product->price,
-                'old_price' => $item->product->getOldPrice(),
-                'current_price' => $item->product->getPrice(),
-                'discount' => $item->product->getSalePercentage(),
-            ]);
-        }
-
-        event(new OrderCreated($order));
+        $order = app(OrderServiceIntarface::class)->store($request, $cart);
 
         Cart::clear();
+
+        event(new OrderCreated($order));
 
         return redirect()->route('cart-final')->with('order_id', $order->id);
     }
