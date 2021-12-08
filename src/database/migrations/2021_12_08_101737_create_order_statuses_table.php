65374b6d0576217f26d08cc90c549f41fd307676
Andrey Dmitrakovich <dmitrakovich.andrey@yandex.by>
2021-12-08 22:46:03 +0300

    create order_statuses & order_item_statuses tables

---
+++ src/database/migrations/2021_12_08_101737_create_order_statuses_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderStatusesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_statuses', function (Blueprint $table) {
+            $table->string('key', 20)->primary();
+            $table->string('name_for_admin', 100);
+            $table->string('name_for_user', 100);
+            $table->integer('sorting')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_statuses');
+    }
+}
