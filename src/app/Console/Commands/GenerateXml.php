baf3a3d863c7e04686e0a8ec297448466bdbfe8e
Andrey Dmitrakovich <dmitrakovich.andrey@yandex.by>
2022-01-21 23:54:56 +0300

    xml: console command

--- src/app/Console/Commands/GenerateXml.php
+++ src/app/Console/Commands/GenerateXml.php
@@ -2,7 +2,13 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Currency;
+use Illuminate\Support\Arr;
+use App\Jobs\XmlGeneratorJob;
+use App\Models\Xml\GoogleXml;
+use App\Models\Xml\YandexXml;
 use Illuminate\Console\Command;
+use Illuminate\Database\Eloquent\Collection as EloquentCollection;
 
 class GenerateXml extends Command
 {
@@ -14,6 +20,14 @@ class GenerateXml extends Command
     protected $signature = 'xml:generate
                             {instance? : Xml instance name}
                             {currency? : Currency for xml}';
+
+    /**
+     * @var array
+     */
+    const INSTANCES = [
+        'yandex' => YandexXml::class,
+        'google' => GoogleXml::class,
+    ];
 
     /**
      * The console command description.
@@ -33,13 +47,60 @@ class GenerateXml extends Command
     }
 
     /**
+     * Get all instances or instance from arguments
+     *
+     * @return array
+     */
+    protected function getInstances(): array
+    {
+        $instances = self::INSTANCES;
+
+        if (!empty($this->argument('instance'))) {
+            $instance = strtolower($this->argument('instance'));
+            $instances = Arr::only($instances, $instance);
+
+            if (empty($instances)) {
+                throw new \Exception('Unknown instance');
+            }
+        }
+
+        return $instances;
+    }
+
+    /**
+     * Get all currencies or currency from argument
+     *
+     * @return EloquentCollection
+     */
+    protected function getCurrencies(): EloquentCollection
+    {
+        $allCurrencies = Currency::all(['code', 'country', 'rate', 'decimals', 'symbol'])->keyBy('code');
+
+        if (!empty($this->argument('currency'))) {
+            $currency = strtoupper($this->argument('currency'));
+            $allCurrencies = $allCurrencies->only($currency);
+
+            if ($allCurrencies->isEmpty()) {
+                throw new \Exception('Unknown currency');
+            }
+        }
+
+        return $allCurrencies;
+    }
+
+    /**
      * Execute the console command.
      *
      * @return int
      */
     public function handle()
     {
-        dd($this->arguments());
+        foreach ($this->getInstances() as $instance) {
+            foreach ($this->getCurrencies() as $currency) {
+                dispatch(new XmlGeneratorJob(new $instance, $currency));
+            }
+        }
+
         return 0;
     }
 }
