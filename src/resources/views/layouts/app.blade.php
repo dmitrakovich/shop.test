97c507dad326f2367c0b934d67b1146b8a68e485
Andrey Dmitrakovich <dmitrievich.a.d@gmail.com>
2022-02-07 17:49:05 +0300

    Google dataLayer: install spatie/laravel-googletagmanager + config

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -32,22 +32,14 @@
         <script src="//code-ya.jivosite.com/widget/paEdMIuNNF" async></script>
 
         {{-- Google Tag Manager --}}
-        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-5PPN2WH');</script>
+        @include('googletagmanager::head')
 
         <!-- Styles -->
         <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     </head>
     <body>
         {{-- Google Tag Manager --}}
-        <noscript>
-            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5PPN2WH"
-                height="0" width="0" style="display:none;visibility:hidden">
-            </iframe>
-        </noscript>
+        @include('googletagmanager::body')
 
         @include('includes.header')
 
