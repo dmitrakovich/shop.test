6b415e98fcab638046e64275a100f4d169b83008
ragon17886 <80809494+ragon17886@users.noreply.github.com>
2022-02-18 04:42:00 +0300

    Update GoogleXml.php

--- src/app/Models/Xml/GoogleXml.php
+++ src/app/Models/Xml/GoogleXml.php
@@ -34,7 +34,7 @@ class GoogleXml extends AbstractXml
     {
         return (object)[
             'channel' => $this->getChannel(),
-            'items' => $this->getItems(),
+            // 'items' => $this->getItems(),
         ];
     }
 
@@ -49,6 +49,7 @@ class GoogleXml extends AbstractXml
             'title' => 'Барокко',
             'link' => $this->getHost(),
             'description' => 'Интернет магазин брендовой обуви',
+            'items' => $this->getItems(),
         ];
     }
 
