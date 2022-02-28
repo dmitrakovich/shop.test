912e0e6efcdf8bffe67f18af3b5f7a5e32461384
ragon17886 <80809494+ragon17886@users.noreply.github.com>
2022-02-28 04:36:26 +0300

    Update head.blade.php

--- src/resources/views/vendor/googletagmanager/head.blade.php
+++ src/resources/views/vendor/googletagmanager/head.blade.php
@@ -1,20 +1,22 @@
 @if($enabled)
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+<script>
+window.dataLayer = window.dataLayer || [];
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','{{ $id }}');</script>
+})(window,document,'script','dataLayer','{{ $id }}');
+</script>
 <script>
 document.addEventListener('DOMContentLoaded', function() {
-    setTimeout(() => {
-        window.dataLayer = window.dataLayer || [];
+    // setTimeout(() => {
         @unless(empty($dataLayer->toArray()))
         window.dataLayer.push({!! $dataLayer->toJson() !!});
         @endunless
         @foreach($pushData as $item)
         window.dataLayer.push({!! $item->toJson() !!});
         @endforeach
-    }, 500);
+    // }, 500);
 });
 </script>
 @endif
