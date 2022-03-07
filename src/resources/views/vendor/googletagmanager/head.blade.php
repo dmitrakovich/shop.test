9074e37d5522aaf0eddfebabefc7d81743ccc1f2
Andrey Dmitrakovich <dmitrievich.a.d@gmail.com>
2022-03-07 14:19:17 +0300

    fix disabled dataLayer

--- src/resources/views/vendor/googletagmanager/head.blade.php
+++ src/resources/views/vendor/googletagmanager/head.blade.php
@@ -1,22 +1,19 @@
-@if($enabled)
 <script>
 window.dataLayer = window.dataLayer || [];
+@if($enabled)
 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','{{ $id }}');
-</script>
-<script>
+
 document.addEventListener('DOMContentLoaded', function() {
-    // setTimeout(() => {
-        @unless(empty($dataLayer->toArray()))
-        window.dataLayer.push({!! $dataLayer->toJson() !!});
-        @endunless
-        @foreach($pushData as $item)
-        window.dataLayer.push({!! $item->toJson() !!});
-        @endforeach
-    // }, 500);
+    @unless(empty($dataLayer->toArray()))
+    window.dataLayer.push({!! $dataLayer->toJson() !!});
+    @endunless
+    @foreach($pushData as $item)
+    window.dataLayer.push({!! $item->toJson() !!});
+    @endforeach
 });
-</script>
 @endif
+</script>
