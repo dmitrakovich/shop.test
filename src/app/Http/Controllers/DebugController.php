f4571498e7d848a5fb9bf64e28139dcb74e0e98c
Andrey Dmitrakovich <dmitrievich.a.d@gmail.com>
2022-03-28 00:26:24 +0300

    format phones in DB

--- src/app/Http/Controllers/DebugController.php
+++ src/app/Http/Controllers/DebugController.php
@@ -2,19 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use App\Facades\Sale;
+use App\Models\User;
 use App\Mail\OrderCreated;
 use App\Models\Orders\Order;
-use App\Models\Product;
-use Exception;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Mail;
+use libphonenumber\PhoneNumberUtil;
+use libphonenumber\PhoneNumberFormat;
+use Illuminate\Database\Eloquent\Model;
 
 class DebugController extends Controller
 {
     public function index()
     {
+        $this->formatPhones();
+        dd(0000);
+
         // php artisan make:mail OrderShipped
 
         $email = 'dmitrakovich.andrey@yandex.by';
@@ -23,16 +24,52 @@ class DebugController extends Controller
         // Mail::to($email)->send(new OrderCreated($order));
 
         return view('emails.order-created', compact('order'));
+    }
+
+    /**
+     * @return void
+     */
+    public function formatPhones()
+    {
+        Order::all()->each(function (Model $order) {
+            $this->formatPhone($order);
+        });
 
 
+        User::all()->each(function (Model $user) {
+            $this->formatPhone($user);
+        });
+    }
 
-        dd(324543);
+    /**
+     * @return void
+     */
+    protected function formatPhone($model, string $phoneFileldName = 'phone', string $countryCode = 'BY')
+    {
+        try {
+            $phone = $model->$phoneFileldName;
+            $phoneUtil = PhoneNumberUtil::getInstance();
 
-        throw new Exception("Test exeption");
+            if (empty($phone)) {
+                return;
+            }
 
-        list($a, $b, $c) = [1, 2];
-        dd($a, $b, $c);
-        dd(Auth::id());
-        dd(Auth::user());
+            $parsedPhone = $phoneUtil->parse($phone, $countryCode);
+
+            if (!$phoneUtil->isValidNumber($parsedPhone)) {
+                return;
+            }
+
+            $formatedPhone = $phoneUtil->format($parsedPhone, PhoneNumberFormat::E164);
+
+            $model->$phoneFileldName = $formatedPhone;
+
+            if ($model->isDirty()) {
+                echo "Old phone: '$phone', new phone: '$formatedPhone'<br>";
+                $model->save();
+            }
+        } catch (\Throwable $th) {
+            dd($th);
+        }
     }
 }
