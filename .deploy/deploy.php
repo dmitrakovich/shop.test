ab93402c14413a0dae021f2889031e6bc584cc7a
Andrey Dmitrakovich <dmitrievich.a.d@gmail.com>
2022-05-09 11:24:04 +0300

    debug deploy 5

--- .deploy/deploy.php
+++ .deploy/deploy.php
@@ -15,7 +15,7 @@ var_dump(getenv());
 var_dump(getenv('TEST_ENV_VAR'));
 var_dump(getenv('PRODUCTION_HOST'));
 var_dump(getenv('PRODUCTION_USERNAME'));
-var_dump(getenv('PRODUCTION_USERNAME123'));
+var_dump(getenv('USERNAME'));
 var_dump(getenv('PRODUCTION_PORT'));
 var_dump(getenv('PRODUCTION_PORT123'));
 var_dump(getenv('PRODUCTION_PATH'));
