927e4b3c70cb6ac2c2971aae37fd0572f9e098f4
Andrey Dmitrakovich <dmitrievich.a.d@gmail.com>
2022-05-13 01:41:58 +0300

    make production & test workflows

--- deploy.php
+++ deploy.php
@@ -41,7 +41,7 @@ host('production')
     ->setIdentityFile('~/.ssh/key.pem');
 
 task('deploy:upload', function () {
-    upload('', '{{release_path}}', ['options' => ['--bwlimit=4096']]);
+    upload('', '{{release_path}}');
 });
 
 // Tasks
