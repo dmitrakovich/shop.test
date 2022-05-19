29b2a2cf48565bc5ba286064ec8e517c64d1ad07
Andrey Dmitrakovich <dmitrievich.a.d@gmail.com>
2022-05-19 11:40:59 +0300

    fix clear cache commands in CacheController

--- src/app/Admin/Controllers/Debug/CacheController.php
+++ src/app/Admin/Controllers/Debug/CacheController.php
@@ -12,10 +12,10 @@ class CacheController extends AdminController
     public function __invoke(Content $content)
     {
         Artisan::call('cache:clear');
-        Artisan::call('config:cache');
-        Artisan::call('route:cache');
-        Artisan::call('view:cache');
-        Artisan::call('event:cache');
+        Artisan::call('config:clear');
+        Artisan::call('route:clear');
+        Artisan::call('view:clear');
+        Artisan::call('event:clear');
         OPcache::clear();
 
         admin_success('Кэш успешно сброшен!');
