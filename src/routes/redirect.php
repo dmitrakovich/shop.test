09fef52350c067ec34f042969efc12b98733c37d
Andrey Dmitrakovich <dmitrievich.a.d@gmail.com>
2022-06-23 11:33:12 +0300

    short-link route

--- src/routes/redirect.php
+++ src/routes/redirect.php
@@ -7,4 +7,4 @@ Route::permanentRedirect('/insta', '/catalog?utm_source=instagram&utm_medium=soc
 Route::permanentRedirect('/tiktok', '/catalog?utm_source=tiktok&utm_medium=social&utm_campaign=accaunt_link');
 
 Route::get('lnk/{short_link:short_link}', fn (ShortLink $shortLink) => redirect($shortLink->full_link))
-    ->missing(fn () => redirect()->route('shop'));
+    ->missing(fn () => redirect()->route('shop'))->name('short-link');
