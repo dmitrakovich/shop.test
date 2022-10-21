93ba7442bee97db0d8190463961917ad9c61f956
Andrey Dmitrakovich <dmitrievich.a.d@gmail.com>
2022-10-21 11:38:31 +0300

    install & setup league/flysystem-aws-s3-v3

--- src/config/filesystems.php
+++ src/config/filesystems.php
@@ -46,13 +46,13 @@ return [
 
         's3' => [
             'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION'),
-            'bucket' => env('AWS_BUCKET'),
-            'url' => env('AWS_URL'),
-            'endpoint' => env('AWS_ENDPOINT'),
-            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+            'key' => env('ACTIVE_STORAGE_ACCESS_KEY_ID'),
+            'secret' => env('ACTIVE_STORAGE_SECRET_ACCESS_KEY'),
+            'region' => env('AWS_DEFAULT_REGION', 'eu-central-1'),
+            'bucket' => env('ACTIVE_STORAGE_BUCKET'),
+            // 'url' => env('AWS_URL'),
+            'endpoint' => env('ACTIVE_STORAGE_ENDPOINT'),
+            // 'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => false,
         ],
 
