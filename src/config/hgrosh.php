72ecd68df57d3f4f2c727469b5d2089db8ecc677
Oneami <alexgodun21@gmail.com>
2022-10-23 23:47:24 +0300

    config hgrosh client

---
+++ src/config/hgrosh.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'token_cache_key'   => 'hgrosh_api_token',
+    'token_url'         => 'https://iii.by/connect/token',
+    'client_id'         => 291311363,
+    'client_secret'     => '2607c51f-d68b-4f92-b092-8cecb9e5ec33',
+    'serviceproviderid' => 18464,
+    'serviceid'         => 1,
+    'retailoutletcode'  => 1,
+
+    'api_url'           => 'https://api-epos.hgrosh.by/public/v1',
+];
