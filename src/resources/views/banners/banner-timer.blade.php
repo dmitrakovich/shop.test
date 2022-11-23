e8346f8db2d804d8cafa1acba0b35d0db9028434
Oneami <alexgodun21@gmail.com>
2022-11-23 23:06:59 +0300

    banner spoiler color

--- src/resources/views/banners/banner-timer.blade.php
+++ src/resources/views/banners/banner-timer.blade.php
@@ -4,7 +4,7 @@
         $timer_id = uniqid();
     @endphp
     <div class="px-3 py-2 d-flex justify-content-between align-items-center"
-        style="background: {{ $banner->spoiler['bg_color'] ?? '#D22020' }}; color: {{ $banner->spoiler['text_color'] ?? '#fff' }};">
+        style="background: {{ $banner->getSpoilerBgColor() }}; color: {{ $banner->getSpoilerTextColor() }};">
         @if (!empty($banner->show_timer) && $banner->show_timer)
             @include('includes.timer', [
                 'end_time' => $banner->end_datetime,
@@ -13,7 +13,7 @@
         @endif
         @if (!empty($banner->spoiler['btn_name']))
             <a class="text-decoration-underline"
-                style="color: {{ $banner->spoiler['text_color'] ?? '#fff' }};" data-toggle="collapse"
+                style="color: {{ $banner->getSpoilerTextColor() }};" data-toggle="collapse"
                 href="#mobCatalogBannerCollapse_{{ $timer_id }}" role="button" aria-expanded="false"
                 aria-controls="mobCatalogBannerCollapse_{{ $timer_id }}">{{ $banner->spoiler['btn_name'] ?? '' }}</a>
         @endif
@@ -21,7 +21,7 @@
     @if (!empty($banner->spoiler['terms']))
         <div class="collapse multi-collapse" id="mobCatalogBannerCollapse_{{ $timer_id }}">
             <div class="card-body"
-                style="background: {{ $banner->spoiler['bg_color'] ?? '#D22020' }}; color: {{ $banner->spoiler['text_color'] ?? '#fff' }};">
+                style="background: {{ $banner->getSpoilerBgColor() }}; color: {{ $banner->getSpoilerTextColor() }};">
                 {!! $banner->spoiler['terms'] ?? '' !!}
             </div>
         </div>
