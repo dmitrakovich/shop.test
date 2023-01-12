509c44bccf5c1bdb05383a5bb3ec0d6e07de6956
Oneami <Oneami@users.noreply.github.com>
2023-01-12 21:17:43 +0000

    Format Oneami code

--- src/app/Notifications/PaymentSms.php
+++ src/app/Notifications/PaymentSms.php
@@ -4,22 +4,22 @@ namespace App\Notifications;
 
 class PaymentSms extends AbstractSmsTraffic
 {
-  /**
-   * Create a new notification instance.
-   *
-   * @return void
-   */
-  public function __construct(
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(
     private string $paymentNum,
     private ?string $firstName = null
   ) {
     }
 
-  /**
-   * Content for sms message
-   */
-  public function getContent(): string
-  {
-    return ($this->firstName ? ($this->firstName . ', ') : '') . 'Вам выставлен счет № ' . $this->paymentNum . ' - подробнее по ссылке ' . route('pay.erip', $this->paymentNum, true);
-  }
+    /**
+     * Content for sms message
+     */
+    public function getContent(): string
+    {
+        return ($this->firstName ? ($this->firstName . ', ') : '') . 'Вам выставлен счет № ' . $this->paymentNum . ' - подробнее по ссылке ' . route('pay.erip', $this->paymentNum, true);
+    }
 }
