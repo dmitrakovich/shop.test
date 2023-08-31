bb2670c166724acbf5c6866792860480e741bb3c
Oneami <Oneami@users.noreply.github.com>
2023-08-31 22:41:59 +0000

    Format Oneami code

--- src/resources/views/includes/feedbacks.blade.php
+++ src/resources/views/includes/feedbacks.blade.php
@@ -29,7 +29,8 @@
                 <div class="col-12 col-md-7">
                     <div class="inc_feedbacks__rating">
                         @for ($i = 1; $i <= 5; $i++)
-                            <span @if($feedback->rating >= $i) class="active" @endif>@include('svg.star')</span>
+                            <span
+                                @if ($feedback->rating >= $i) class="active" @endif>@include('svg.star')</span>
                         @endfor
                     </div>
                     <p>{{ $feedback->text }}</p>
@@ -106,8 +107,8 @@
             </label>
             <div class="col-12 col-md-8">
                 <input type="text" name="user_name" id="inputName" class="form-control"
-                    value="{{ optional(auth()->user())->first_name }}" autocomplete="given-name" placeholder="Имя"
-                    required>
+                    value="{{ optional(auth()->user())->first_name }}" autocomplete="given-name"
+                    placeholder="Имя" required>
             </div>
         </div>
 
@@ -117,8 +118,8 @@
             </label>
             <div class="col-12 col-md-8">
                 <input type="text" name="user_city" id="inputCity" class="form-control"
-                    value="{{ optional(auth()->user())->getFirstAddress()?->city }}" autocomplete="address-level2"
-                    placeholder="Город" required>
+                    value="{{ optional(auth()->user())->getFirstAddress()?->city }}"
+                    autocomplete="address-level2" placeholder="Город" required>
             </div>
         </div>
 
@@ -127,8 +128,8 @@
                 <b>Загрузите фотографии</b>
             </label>
             <div class="col-12 col-md-8">
-                <input type="file" accept="image/*" name="photos[]" id="inputPhotos" class="form-control-file"
-                    multiple>
+                <input type="file" accept="image/*" name="photos[]" id="inputPhotos"
+                    class="form-control-file" multiple>
             </div>
         </div>
 
@@ -137,8 +138,8 @@
                 <b>Загрузите видео</b>
             </label>
             <div class="col-12 col-md-8">
-                <input type="file" accept="video/*" name="videos[]" id="inputVideos" class="form-control-file"
-                    multiple>
+                <input type="file" accept="video/*" name="videos[]" id="inputVideos"
+                    class="form-control-file" multiple>
             </div>
         </div>
 
--- src/resources/views/shop/product.blade.php
+++ src/resources/views/shop/product.blade.php
@@ -49,7 +49,8 @@
                                         </div>
                                     @else
                                         <div class="swiper-slide">
-                                            <a href="{{ $image->getUrl('full') }}" data-fancybox="images">
+                                            <a href="{{ $image->getUrl('full') }}"
+                                                data-fancybox="images">
                                                 <img src="{{ $image->getUrl('normal') }}"
                                                     alt="{{ $product->shortName() }}" class="img-fluid"
                                                     onerror="imageOnError(this)">
@@ -73,7 +74,8 @@
                         <h1 class="col-6 text-muted p-product__title">
                             {{ $product->shortName() }}
                         </h1>
-                        <div class="col-6 p-product__rating js-scrollTo" data-scroll-to="#productFeedbacks">
+                        <div class="col-6 p-product__rating js-scrollTo"
+                            data-scroll-to="#productFeedbacks">
                             @for ($i = 1; $i <= 5; $i++)
                                 <span
                                     @if ($feedbacks->avg('rating') >= $i) class="active" @endif>@include('svg.star')</span>
@@ -84,7 +86,8 @@
                     </div>
                 </div>
 
-                <form class="col-12 col-xl-10" id="product-info" action=" {{ route('cart-add') }}" method="post">
+                <form class="col-12 col-xl-10" id="product-info" action=" {{ route('cart-add') }}"
+                    method="post">
                     <input type="hidden" name="product_id" id="product_id" value="{{ $product->id }}">
                     <div class="row mt-4">
                         {{-- blade-formatter-disable-next-line --}}
@@ -105,7 +108,8 @@
                                     <div class="price price-new font-weight-bold col-auto">
                                         <b>-{{ $product->getSalePercentage() }}%</b>
                                     </div>
-                                    <button type="button" class="btn sale-help-trigger px-1" data-toggle="dropdown">
+                                    <button type="button" class="btn sale-help-trigger px-1"
+                                        data-toggle="dropdown">
                                         <div class="tooltip-trigger">?</div>
                                     </button>
                                     <div class="dropdown-menu font-size-12 sale-help-block px-3 py-2">
@@ -157,8 +161,8 @@
                                         <span class="text-muted">Выберите размер:</span>
                                     </div>
                                     <div class="col-auto">
-                                        <a class="text-decoration-underline" data-fancybox data-src="#size-table"
-                                            href="javascript:;">
+                                        <a class="text-decoration-underline" data-fancybox
+                                            data-src="#size-table" href="javascript:;">
                                             Таблица размеров
                                         </a>
                                     </div>
@@ -170,7 +174,8 @@
                                                 <span class="checkmark">{{ $size->name }}</span>
                                             </label>
                                             <input type="checkbox" id="input-size-{{ $size->id }}"
-                                                class="visually-hidden" name="sizes[{{ $size->id }}]" />
+                                                class="visually-hidden"
+                                                name="sizes[{{ $size->id }}]" />
                                         </li>
                                     @endforeach
                                 </ul>
@@ -179,7 +184,8 @@
 
                         <div class="row justify-content-center">
                             <div class="col-12">
-                                <button type="button" class="btn btn-dark btn-lg btn-block js-add-to-cart py-3">
+                                <button type="button"
+                                    class="btn btn-dark btn-lg btn-block js-add-to-cart py-3">
                                     В КОРЗИНУ
                                 </button>
                                 <button type="button"
@@ -229,8 +235,9 @@
                 </form>
                 @if (!empty($product->availableSizes))
                     <div class="p-product__stock col-12 mt-4">
-                        <h4 class="p-product__stock-title" data-toggle="collapse" href="#productStockInfo"
-                            role="button" aria-expanded="false" aria-controls="productStockInfo">
+                        <h4 class="p-product__stock-title" data-toggle="collapse"
+                            href="#productStockInfo" role="button" aria-expanded="false"
+                            aria-controls="productStockInfo">
                             Наличие в магазинах
                             <span class="p-product__stock-title_icon"></span>
                         </h4>
@@ -369,20 +376,23 @@
     {{-- modals --}}
     <div style="display: none;" id="buy-one-click" class="row">
 
-        <form action="{{ route('orders.store') }}" method="post" class="col-12 text-center" id="oneclick-form">
+        <form action="{{ route('orders.store') }}" method="post" class="col-12 text-center"
+            id="oneclick-form">
             @csrf
             <input type="hidden" name="product_id" value="{{ $product->id }}">
             <h3 class="mx-5 mb-4">Купить в один клик</h3>
             <div class="form-group">
                 <input type="text" class="form-control" name="first_name" placeholder="Имя"
-                    value="{{ optional(auth()->user())->first_name }}" autocomplete="given-name" required>
+                    value="{{ optional(auth()->user())->first_name }}" autocomplete="given-name"
+                    required>
             </div>
             <div class="form-group">
                 @include('partials.inputs.phone')
             </div>
             <div class="form-group">
-                <input type="text" class="form-control" name="user_addr" placeholder="Населенный пункт"
-                    autocomplete="address" required value="{{ optional(auth()->user())->getFirstFullAddress() }}">
+                <input type="text" class="form-control" name="user_addr"
+                    placeholder="Населенный пункт" autocomplete="address" required
+                    value="{{ optional(auth()->user())->getFirstFullAddress() }}">
             </div>
             <button type="button" class="btn btn-dark my-3 px-5" id="buy-one-click-submit">
                 Купить
