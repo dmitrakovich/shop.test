f37c65e896644b917703c2b1f3fbe60fbcbb488b
Oneami <alexgodun21@gmail.com>
2023-09-11 10:27:32 +0300

    83. fix in belpost label && installmet

--- src/app/Services/Order/InstallmentOrderService.php
+++ src/app/Services/Order/InstallmentOrderService.php
@@ -21,7 +21,9 @@ class InstallmentOrderService
         $resultPath = '/storage/order_installments/' . $order->id . '.xlsx';
         File::ensureDirectoryExists(dirname(public_path($resultPath)));
         $order->loadMissing([
+            'admin',
             'user.passport',
+            'onlinePayments',
             'items' => fn ($query) => $query
                 ->where('status_key', 'pickup')
                 ->with('installment'),
@@ -33,6 +35,8 @@ class InstallmentOrderService
         $patronymicName = ($order->patronymic_name ?? $order->user->patronymic_name ?? null);
         $firstSheet = clone $spreadsheet->getActiveSheet();
 
+        $onlinePaymentsSum = $order->getAmountPaidOrders();
+        $uniqItemsCount = $order->getUniqItemsCount();
         foreach ($order->items as $itemKey => $item) {
             if ($itemKey > 0) {
                 $spreadsheet->addSheet($firstSheet);
@@ -41,23 +45,32 @@ class InstallmentOrderService
             $sheet = $spreadsheet->getActiveSheet()->setTitle('№' . $itemKey + 1);
 
             $sheet->setCellValue('AD1', $item->installment->contract_number ?? null);
-            $sheet->setCellValue('AL3', Carbon::parse(now())->translatedFormat('l, F j, Y'));
-            $sheet->setCellValue('I7', $firstName);
-            $sheet->setCellValue('T7', $lastName);
+
+            $itemPrice = $item->current_price;
+            $itemPrice += $order->delivery_price ? ($order->delivery_price / $uniqItemsCount) : 0;
+            $itemPrice -= $onlinePaymentsSum ? ($onlinePaymentsSum / $uniqItemsCount) : 0;
+            $adminFio = $order?->admin?->user_last_name . ' ' . mb_strtoupper(mb_substr($order?->admin?->name, 0, 1)) . '.' . mb_strtoupper(mb_substr($order?->admin?->user_patronymic_name, 0, 1)) . '.';
+            $sheet->unmergeCells('AL3:AW3');
+            $sheet->mergeCells('AI3:AX3');
+            $sheet->setCellValue('AI3', ('"' . Carbon::parse(now())->translatedFormat('l, F j, Y') . '"'));
+            $sheet->setCellValue('E5', 'Общество с ограниченной ответственностью "БароккоСтайл", в лице директора');
+            $sheet->setCellValue('B6', $adminFio . ', действующий на основании Устава, именуемый в дальнейшем Продавец, с одной');
+            $sheet->setCellValue('I7', $lastName);
+            $sheet->setCellValue('T7', $firstName);
             $sheet->setCellValue('AC7', $patronymicName);
 
             $sheet->setCellValue('C11', ($item->product->brand->name ?? null) . ', ' . mb_strtolower($item->product->category->name ?? ''));
             $sheet->setCellValue('AD11', $item->product->sku ?? $item->product->title ?? null);
             $sheet->setCellValue('G12', $item->size->name ?? null);
-            $sheet->setCellValue('H13', TextHelper::numberToMoneyShortString($item->current_price));
-            $sheet->setCellValue('V13', TextHelper::numberToMoneyString($item->current_price));
+            $sheet->setCellValue('H13', TextHelper::numberToMoneyShortString($itemPrice));
+            $sheet->setCellValue('V13', TextHelper::numberToMoneyString($itemPrice));
 
             $sheet->setCellValue('J17', date('m/d/Y', strtotime('now')));
             $sheet->setCellValue('J18', date('m/d/Y', strtotime('+1 month')));
             $sheet->setCellValue('J19', date('m/d/Y', strtotime('+1 month')));
             $sheet->setCellValue('J20', date('m/d/Y', strtotime('+2 month')));
 
-            $sheet->setCellValue('X16', ($item->current_price - (($item->installment->monthly_fee ?? 0) * 2)));
+            $sheet->setCellValue('X16', ($itemPrice - (($item->installment->monthly_fee ?? 0) * 2)));
             $sheet->setCellValue('X17', $item->installment->monthly_fee ?? null);
             $sheet->setCellValue('X19', $item->installment->monthly_fee ?? null);
 
@@ -83,6 +96,7 @@ class InstallmentOrderService
             $sheet->setCellValue('AH47', substr(trim($order->phone), -9, -7));
             $sheet->setCellValue('AL47', substr(trim($order->phone), -7));
             $sheet->setCellValue('AK52', mb_strtoupper(mb_substr($firstName, 0, 1)) . '.' . mb_strtoupper(mb_substr($patronymicName, 0, 1)) . '. ' . $lastName);
+            $sheet->setCellValue('L52', mb_strtoupper(mb_substr($order?->admin?->name, 0, 1)) . '.' . mb_strtoupper(mb_substr($order?->admin?->user_patronymic_name, 0, 1)) . '. ' . $order?->admin?->user_last_name);
 
             $item->installment->installment_form_file = $resultPath;
             $item->installment->save();
