f9f4d7e24f426b55a40fdf4ab68b5823e6c193c8
Oneami <Oneami@users.noreply.github.com>
2023-11-28 20:56:49 +0000

    Format Oneami code

--- src/database/migrations/2023_11_22_230543_add_auto_order_statuses_key_to_configs_table.php
+++ src/database/migrations/2023_11_22_230543_add_auto_order_statuses_key_to_configs_table.php
@@ -1,10 +1,7 @@
 <?php
 
 use App\Models\Config;
-
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
