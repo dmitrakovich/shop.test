0e689a31d99916fe241c6feb33fe4e6d536848ba
Oneami <Oneami@users.noreply.github.com>
2023-12-04 08:03:18 +0000

    Format Oneami code

--- src/config/services.php
+++ src/config/services.php
@@ -52,5 +52,5 @@ return [
         'belpost_host' => env('IMAP_BELPOST_HOST'),
         'belpost_user' => env('IMAP_BELPOST_USER'),
         'belpost_password' => env('IMAP_BELPOST_PASSWORD'),
-    ]
+    ],
 ];
