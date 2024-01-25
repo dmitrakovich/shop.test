427d4215de94569f0bd3cbe6018b2efce22d5859
Andrey Dmitrakovich <dmitrakovich.andrey@yandex.by>
2024-01-25 23:03:24 +0300

    38. update browserslist, disable Performance Monitoring, fix github action

--- src/resources/js/sentry.js
+++ src/resources/js/sentry.js
@@ -4,5 +4,5 @@ Sentry.init({
   dsn: 'https://29c9b17aba169d65d6ce3570ffb78738@o4505659571961856.ingest.sentry.io/4506633176154112',
   integrations: [],
   // Performance Monitoring
-  tracesSampleRate: 0.1,
+  // tracesSampleRate: 1,
 });
