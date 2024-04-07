77995e75188a24add25201070a12b8e66412f9e1
dmitrakovich <dmitrakovich@users.noreply.github.com>
2024-04-07 18:08:55 +0000

    Format dmitrakovich code

--- src/public/index.php
+++ src/public/index.php
@@ -16,5 +16,5 @@ require_once __DIR__ . '/../app/Helpers/functions.php';
 require __DIR__ . '/../vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
-(require_once __DIR__.'/../bootstrap/app.php')
+(require_once __DIR__ . '/../bootstrap/app.php')
     ->handleRequest(Request::capture());
