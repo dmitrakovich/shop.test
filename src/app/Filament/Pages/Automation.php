cd3224a92b0840a8d5dfed3eed2036e4a2702a74
Oneami <alexgodun21@gmail.com>
2024-06-28 00:27:07 +0300

    New admin: clients

---
+++ src/app/Filament/Pages/Automation.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Filament\Pages\Page;
+
+use Illuminate\Contracts\View\View;
+
+class Automation extends Page
+{
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+
+    protected static string $view = 'filament.pages.automation';
+
+    protected static ?string $navigationGroup = 'automation';
+
+    protected static ?string $title = 'Пыщ пыщ';
+
+
+}
