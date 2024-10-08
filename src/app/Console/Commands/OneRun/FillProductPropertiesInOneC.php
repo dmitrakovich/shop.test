b9bc1fac88ec9ba7ce6448ca14fa5e843b5eae7b
Andrey Dmitrakovich <dmitrakovich.andrey@yandex.by>
2024-10-08 22:37:27 +0300

    45. update command for fill product properties in 1C

--- src/app/Console/Commands/OneRun/FillProductPropertiesInOneC.php
+++ src/app/Console/Commands/OneRun/FillProductPropertiesInOneC.php
@@ -8,14 +8,14 @@ use App\Models\Product;
 use Illuminate\Console\Command;
 use Illuminate\Database\Eloquent\Collection;
 
-class FillPhotoAndUrlInOneCProducts extends Command
+class FillProductPropertiesInOneC extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'one-run:fill-photo-and-url-in-one-c-products';
+    protected $signature = 'one-run:fill-product-properties-in-one-c';
 
     /**
      * The console command description.
@@ -35,8 +35,8 @@ class FillPhotoAndUrlInOneCProducts extends Command
         $productUpdater = new UpdateProduct();
 
         $productsQuery
-            ->with(['category', 'productFromOneC', 'media', 'manufacturer', 'countryOfOrigin'])
-            ->select(['id', 'one_c_id', 'slug', 'category_id', 'manufacturer_id', 'country_of_origin_id'])
+            ->with(['category', 'productFromOneC', 'media', 'manufacturer', 'collection', 'countryOfOrigin'])
+            ->select(['id', 'one_c_id', 'slug', 'category_id', 'manufacturer_id', 'collection_id', 'country_of_origin_id'])
             ->chunk(200, function (Collection $chunk) use ($productUpdater) {
                 $chunk->each(function (Product $product) use ($productUpdater) {
                     $productUpdater->handle(new ProductUpdated($product));
