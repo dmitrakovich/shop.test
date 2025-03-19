5cf181b03fa94aa19f3340e68f4d69ecff2f9ebf
Andrey Dmitrakovich <dmitrakovich.andrey@yandex.by>
2025-03-19 23:30:42 +0300

    85. AppResource

---
+++ src/app/Facades/Favorite.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Facades;
+
+use App\Models\Favorite as FavoriteModel;
+use App\Models\Product;
+use App\Services\FavoriteService;
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * @method static Collection<int,Product> getProducts()
+ * @method static FavoriteModel addProduct(Product $product)
+ * @method static void removeProduct(Product $product)
+ * @method static void toggleProduct(Product $product)
+ *
+ * @see FavoriteService
+ */
+class Favorite extends Facade
+{
+    protected static function getFacadeAccessor(): string
+    {
+        return FavoriteService::class;
+    }
+}
---
+++ src/app/Http/Resources/AppResource.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Facades\Cart;
+use App\Facades\Currency;
+use App\Facades\Favorite;
+use App\Http\Resources\Cart\CartResource;
+use App\Http\Resources\Favorite\FavoritesResource;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AppResource extends JsonResource
+{
+    /**
+     * Create a new resource instance.
+     */
+    public function __construct() {}
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'cart' => new CartResource(Cart::getCart()),
+            'favorites' => new FavoritesResource(Favorite::getProducts()),
+            'currency' => Currency::getCurrentCurrency(),
+            'contacts' => [
+                'phone_main' => '+375 (29) 179-37-90',
+                'phone_by' => '+375 (29) 522-77-22',
+                'phone_ru' => '8-800-100-77-69',
+                'email' => config('contacts.email.link'),
+                'telegram' => config('contacts.telegram.link'),
+                'telegram_channel' => config('contacts.telegram-channel.link'),
+                'viber' => config('contacts.viber.link'),
+                'viber_channel' => config('contacts.viber-channel.link'),
+                'whats_app' => config('contacts.whats-app.link'),
+                'instagram' => config('contacts.instagram.link'),
+            ],
+            'work_time' => 'Ежедневно с 08:00 до 21:00',
+        ];
+    }
+}
