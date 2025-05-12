2cb9ec77823ba1fd2d7f287b3743724e9998d707
Andrey Dmitrakovich <dmitrakovich.andrey@yandex.by>
2025-05-12 23:17:06 +0300

    12. add stock_id & soft delete

--- src/app/Models/DefectiveProduct.php
+++ src/app/Models/DefectiveProduct.php
@@ -4,22 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
  * @property int $id
  * @property int $product_id id товара
  * @property int $size_id id размера
+ * @property int $stock_id id склада
  * @property string|null $reason Причина добавления в брак
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property \Illuminate\Support\Carbon|null $deleted_at
  *
  * @property-read \App\Models\Product|null $product
  * @property-read \App\Models\Size|null $size
+ * @property-read \App\Models\Stock|null $stock
  *
  * @mixin \Illuminate\Database\Eloquent\Builder
  */
 class DefectiveProduct extends Model
 {
+    use SoftDeletes;
+
     /**
      * The attributes that aren't mass assignable.
      *
@@ -41,5 +47,13 @@ class DefectiveProduct extends Model
     public function size(): BelongsTo
     {
         return $this->belongsTo(Size::class);
+    }
+
+    /**
+     * Get the stock associated with the defective product.
+     */
+    public function stock(): BelongsTo
+    {
+        return $this->belongsTo(Stock::class);
     }
 }
