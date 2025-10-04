9d6b2685cadf82929a9ee1d5428017c400175ab4
Andrey Dmitrakovich <dmitrakovich.andrey@yandex.by>
2025-10-04 18:55:28 +0300

    156. new products admin panel (WIP)

--- src/app/Filament/Resources/Products/Products/Schemas/ProductForm.php
+++ src/app/Filament/Resources/Products/Products/Schemas/ProductForm.php
@@ -4,11 +4,12 @@ namespace App\Filament\Resources\Products\Products\Schemas;
 
 use Filament\Forms\Components\RichEditor;
 use Filament\Forms\Components\Select;
+use Filament\Forms\Components\SpatieMediaLibraryFileUpload;
 use Filament\Forms\Components\TextInput;
-use Filament\Forms\Components\Toggle;
 use Filament\Schemas\Components\Group;
 use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
+use Illuminate\Database\Eloquent\Builder;
 
 class ProductForm
 {
@@ -25,7 +26,7 @@ class ProductForm
                                     ->required(),
                                 TextInput::make('slug')
                                     ->default('temp_slug_' . time())
-                                    ->disabled(),
+                                    ->readOnly(),
                                 RichEditor::make('description')
                                     ->label('Описание')
                                     ->columnSpanFull(),
@@ -47,62 +48,111 @@ class ProductForm
                                     ->prefix('BYN'),
                             ])
                             ->columns(2),
+                        Section::make('Фото')
+                            ->schema([
+                                SpatieMediaLibraryFileUpload::make('media')
+                                    ->image()
+                                    ->multiple()
+                                    ->panelLayout('grid')
+                                    ->reorderable()
+                                    ->downloadable()
+                                    ->hiddenLabel(),
+                            ])
+                            ->collapsible(),
                     ])
                     ->columnSpan(['lg' => 2]),
                 Group::make()
                     ->schema([
-                        Section::make()
+                        Section::make('Характеристики')
                             ->schema([
-                                TextInput::make('one_c_id')
-                                    ->numeric(),
-                            ])
-                            ->columns(2),
+                                // TextInput::make('one_c_id'),
+                                // TextInput::make('rating'),
+                                Select::make('category_id')
+                                    ->label('Категория')
+                                    ->relationship('category', 'title')
+                                    ->searchable()
+                                    ->preload()
+                                    ->required(),
+                                Select::make('brand_id')
+                                    ->label('Бренд')
+                                    ->relationship('brand', 'name')
+                                    ->searchable()
+                                    ->preload()
+                                    ->required(),
+                                Select::make('manufacturer_id')
+                                    ->label('Производитель')
+                                    ->relationship('manufacturer', 'name')
+                                    ->searchable()
+                                    ->preload()
+                                    ->required(),
+                                Select::make('sizes')
+                                    ->label('Размеры')
+                                    ->multiple()
+                                    ->relationship('sizes', 'name', function (Builder $query) {
+                                        $query->where('is_active', true);
+                                    })
+                                    ->preload()
+                                    ->required(),
+                                Select::make('collection_id')
+                                    ->label('Коллекция')
+                                    ->relationship('collection', 'name', function (Builder $query) {
+                                        $query->orderByDesc('id');
+                                    })
+                                    ->native(false)
+                                    ->required(),
+                                Select::make('season_id')
+                                    ->label('Сезон')
+                                    ->relationship('season', 'name')
+                                    ->required()
+                                    ->native(false),
+                                Select::make('colors')
+                                    ->label('Цвет для фильтра')
+                                    ->multiple()
+                                    ->relationship('colors', 'name')
+                                    ->preload(),
+                                Select::make('fabrics')
+                                    ->label('Материал для фильтра')
+                                    ->multiple()
+                                    ->relationship('fabrics', 'name')
+                                    ->preload(),
+                                Select::make('styles')
+                                    ->label('Стиль')
+                                    ->multiple()
+                                    ->relationship('styles', 'name')
+                                    ->preload(),
+                                Select::make('heels')
+                                    ->label('Тип каблука/подошвы')
+                                    ->multiple()
+                                    ->relationship('heels', 'name')
+                                    ->preload(),
+                                Select::make('country_of_origin_id')
+                                    ->label('Страна производитель')
+                                    ->relationship('countryOfOrigin', 'name')
+                                    ->native(false),
+                            ]),
+                        Section::make('Описание')
+                            ->schema([
+                                TextInput::make('color_txt')->label('Цвет'),
+                                TextInput::make('fabric_top_txt')->label('Материал верха'),
+                                TextInput::make('fabric_inner_txt')->label('Материал внутри'),
+                                TextInput::make('fabric_insole_txt')->label('Материал стельки'),
+                                TextInput::make('fabric_outsole_txt')->label('Материал подошвы'),
+                                TextInput::make('bootleg_height_txt')->label('Высота голенища'),
+                                TextInput::make('heel_txt')->label('Высота каблука/подошвы'),
+                            ]),
 
                     ])
                     ->columnSpan(['lg' => 1]),
 
+                // todo:
                 TextInput::make('label_id')
                     ->required()
                     ->numeric(),
-                Select::make('category_id')
-                    ->relationship('category', 'title')
-                    ->required()
-                    ->default(0),
-                Select::make('season_id')
-                    ->relationship('season', 'name')
-                    ->required()
-                    ->default(0),
-                Select::make('brand_id')
-                    ->relationship('brand', 'name')
-                    ->required()
-                    ->default(0),
-                Select::make('manufacturer_id')
-                    ->relationship('manufacturer', 'name')
-                    ->required()
-                    ->default(0),
-                Select::make('collection_id')
-                    ->relationship('collection', 'name')
-                    ->required()
-                    ->default(0),
-                TextInput::make('color_txt'),
-                TextInput::make('fabric_top_txt'),
-                TextInput::make('fabric_inner_txt'),
-                TextInput::make('fabric_insole_txt'),
-                TextInput::make('fabric_outsole_txt'),
-                TextInput::make('heel_txt'),
-                TextInput::make('bootleg_height_txt'),
-                Toggle::make('action')
-                    ->required(),
-                TextInput::make('rating')
-                    ->required()
-                    ->numeric()
-                    ->default(0),
                 Select::make('product_group_id')
                     ->relationship('productGroup', 'id'),
                 TextInput::make('product_features'),
                 TextInput::make('key_features'),
-                Select::make('country_of_origin_id')
-                    ->relationship('countryOfOrigin', 'name'),
+
             ])
             ->columns(3);
     }
