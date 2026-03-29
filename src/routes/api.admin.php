d2f22e985da8bc7f6ac198c67b0200b5048250ae
Andrey Dmitrakovich <dmitrakovich.andrey@yandex.by>
2026-03-29 20:58:21 +0300

    218. remove CropperController & frontend sass files

--- src/routes/api.admin.php
+++ src/routes/api.admin.php
@@ -2,7 +2,6 @@
 
 use App\Admin\Controllers\Api\ProductController;
 use App\Admin\Controllers\Api\StocksController;
-use App\Http\Controllers\CropperController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('product')->group(function () {
@@ -10,6 +9,3 @@ Route::prefix('product')->group(function () {
     Route::get('data', [ProductController::class, 'getProductDataById']);
 });
 Route::get('stocks', [StocksController::class, 'get']);
-
-// Route::post('croppic/save', [CropperController::class, 'save']);
-Route::post('croppic/crop', [CropperController::class, 'crop']);
