0c2c6fccf8d6be57e9a6c3c1d555514ca8306729
Andrey Dmitrakovich <dmitrakovich.andrey@yandex.by>
2026-07-30 22:30:48 +0300

    324. fix config for ES certs

--- src/config/elastic.client.php
+++ src/config/elastic.client.php
@@ -9,13 +9,11 @@ return [
             'hosts' => [
                 env('ELASTIC_HOST', 'localhost:9200'),
             ],
-            // configure basic authentication
             'basicAuthentication' => [
                 env('ELASTIC_USERNAME'),
                 env('ELASTIC_PASSWORD'),
             ],
-            // bool, or absolute path to Elasticsearch HTTP CA (e.g. http_ca.crt)
-            'sslVerification' => env('ELASTIC_SSL_CA') ?: true,
+            'CABundle' => env('ELASTIC_SSL_CA', ''),
         ],
     ],
 ];
